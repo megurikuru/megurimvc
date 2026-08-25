b1c1b892c9180d931e2c5d7d5dbb5c972f5cf2c8
meguri <meguri@meguri.info>
2026-08-26 06:05:14 +0900

    Line break position for parentheses

--- meguri/Models/ErrorViewModel.cs
+++ meguri/Models/ErrorViewModel.cs
@@ -1,9 +1,7 @@
 using System;
 
-namespace meguri.Models
-{
-    public class ErrorViewModel
-    {
+namespace meguri.Models {
+    public class ErrorViewModel {
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
--- meguri/Services/EmailSender.cs
+++ meguri/Services/EmailSender.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace meguri.Services
-{
+namespace meguri.Services {
     // This class is used by the application to send email for account confirmation and password reset.
     // For more details see https://go.microsoft.com/fwlink/?LinkID=532713
-    public class EmailSender : IEmailSender
-    {
-        public Task SendEmailAsync(string email, string subject, string message)
-        {
+    public class EmailSender : IEmailSender {
+        public Task SendEmailAsync(string email, string subject, string message) {
             return Task.CompletedTask;
         }
     }
