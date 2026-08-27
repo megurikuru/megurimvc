e3a1c4259b1c53e9f8d71dc2126e08cfa28e55ab
meguri <meguri@meguri.info>
2026-08-27 22:30:37 +0900

    Changed the database to PostgreSQL

--- meguri/Program.cs
+++ meguri/Program.cs
@@ -1,18 +1,27 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Meguri.Data;
+using Meguri.Models;
+using Meguri.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Meguri.Data;
-using Meguri.Models;
-using Meguri.Services;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Database
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+var connectionString = builder.Configuration.GetConnectionString(
+    "DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found."
+);
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseNpgsql(connectionString)
+);
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
