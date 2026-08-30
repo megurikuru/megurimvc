732363743fe62ff823444938861a85a626094883
meguri <meguri@meguri.info>
2026-08-30 20:31:58 +0900

    Capitalize the first letter of the project file name.

--- Meguri/publish.sh
+++ Meguri/publish.sh
@@ -2,7 +2,7 @@ systemctl stop kestrel-meguri.service
 
 dotnet publish --configuration Release
 rm -rf /var/www/meguri
-cp -pr bin/Release/net10.0/publish /var/www/meguri
-chown www-data:www-data /var/www/meguri
+cp -pr /home/meguri/repos/megurimvc/Meguri/bin/Release/net10.0/publish /var/www/meguri
+chown -R www-data:www-data /var/www/meguri
 
 systemctl start kestrel-meguri.service
